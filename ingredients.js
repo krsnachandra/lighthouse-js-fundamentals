b78254770d243b7f9a4d60363e10e875b726d8ea
Chandra <krsna.c@gmail.com>
2017-10-16 20:46:00 +0000

    Added 2 LoopyLighthouse exercises

--- ingredients.js
+++ ingredients.js
@@ -14,3 +14,47 @@ for (var i = 0; i < ingredients.length; i++) {
 for (var i = ingredients.length - 1; i >= 0; i--) {
   console.log(ingredients[i]);
 }
+
+
+function range(start, end, step) {
+  var array = [];
+  if (end > start && step > 0) {
+    for (var i = start; i <= end; i += step) {
+      array.push(i);
+    }
+  }
+  return array;
+}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+console.log(range(2, 4));
+console.log(range(10, 8, 2));
+console.log(range(1, 1, -1));
+
+
+function lastIndexOf(array, value) {
+  var match = false;
+  for (var i = array.length - 1; i >= 0; i--) {
+    if (array[i] === value) {
+      var result = i;
+      match = true;
+      break;
+    }
+  }
+  if (match) {
+    return result;
+  }
+  else {
+    return -1;
+  }
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
+console.log(lastIndexOf([1], 3), "=?", -1);
+console.log(lastIndexOf([3], 3), "=?", 0);
